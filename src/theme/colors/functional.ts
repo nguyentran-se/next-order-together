9ca75c418fb5ec5cd35628d08a3aca5e4acb053c
Bao Pham <bao_pham@datahouse.com>
2023-08-25 23:11:05 -1000

    feat: set up MUI

---
+++ src/theme/colors/functional.ts
@@ -0,0 +1,12 @@
+export const functionalColors = {
+  info: '#1A74DC',
+  infoBg: '#E1F6FD',
+  success: '#10892A',
+  successBg: '#D2FBD6',
+  error: '#F30018',
+  errorBg: '#FEE0E0',
+  warning: '#E35505',
+  warningBg: '#F9E8D9',
+  white: '#FFFFFF',
+  hover: '#F3FAFF',
+};
