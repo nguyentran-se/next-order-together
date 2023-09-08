09678881a194ea195461e73d63f3fa75414a636e
Bao Pham <bao_pham@datahouse.com>
2023-09-07 22:51:04 -1000

    feat: get room detail, fix styles

---
+++ src/app/rooms/[id]/RoomInfo.tsx
@@ -0,0 +1,50 @@
+import { Typography, Box, Stack } from '@mui/material';
+import WatchLaterIcon from '@mui/icons-material/WatchLater';
+import PlaceIcon from '@mui/icons-material/Place';
+import SellIcon from '@mui/icons-material/Sell';
+import { IRoom } from '@/app/_interfaces';
+import { getRoomDetailFromRoomData } from '../room-utils';
+
+function RoomInfo({ roomInfo }: { roomInfo: IRoom }) {
+  const roomDetail = getRoomDetailFromRoomData(roomInfo);
+  return (
+    <>
+      <Stack direction="row">
+        {/* Room name */}
+        <Typography variant="h4" fontWeight="500" sx={{ mr: 1 }}>
+          {roomDetail.name}
+        </Typography>
+
+        {/* Due datetime */}
+        <Box
+          sx={{
+            backgroundColor: '#FF7C02',
+            borderRadius: '5px',
+          }}
+        >
+          <Typography display="flex" alignItems="center" color="common.white" padding="12px">
+            <WatchLaterIcon sx={{ mr: 1 }} />
+            {`Close order at ${roomInfo.dueTime}`}
+          </Typography>
+        </Box>
+      </Stack>
+
+      {/* Description */}
+      <Typography color="text.secondary">{roomDetail.description}</Typography>
+
+      {/* Address and price range */}
+      <Typography component="div">
+        <Typography>
+          <PlaceIcon />
+          {roomDetail.address.street || 'Somewhere in the world'}
+        </Typography>
+        <Typography>
+          <SellIcon />
+          {`Price range (on working)`}
+        </Typography>
+      </Typography>
+    </>
+  );
+}
+
+export default RoomInfo;
---
+++ src/hooks/useGetRoom.ts
@@ -0,0 +1,12 @@
+import { getRoom } from '@/apis/queries';
+import { IRoom } from '@/app/_interfaces';
+import { useQuery } from '@tanstack/react-query';
+
+export const useGetRoom = (id: string) => {
+  return useQuery<IRoom>({
+    queryKey: [`get-room-${id}`],
+    queryFn: () => getRoom(id),
+    retry: 3,
+    staleTime: 1000*5,
+  });
+};
