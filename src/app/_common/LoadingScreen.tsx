8eac6645ecadc0d745b7da2b2514d5058f7bd174
Bao Pham <bao_pham@datahouse.com>
2023-09-13 23:35:03 -1000

    fix: fix loading screen

--- src/app/_common/LoadingScreen.tsx
+++ src/app/_common/LoadingScreen.tsx
@@ -1,5 +1,6 @@
 import { Box } from '@mui/material';
 import Image from 'next/image';
+import giphy from './giphy.gif';
 
 const styles = {
   '@keyframes sparkle': {
@@ -18,10 +19,10 @@ function LoadingScreen() {
   const __DEVMODE__ = process.env.NODE_ENV === 'development';
   const fixedSrc =
     'https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExZ3Fwc3NzYjY4cHVwajl2OHk3a3JqbndrZjI5ZzFpZ29yaHY3c2tuaCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/ZBQhoZC0nqknSviPqT/giphy.gif';
-  const loadingSrc = __DEVMODE__ ? fixedSrc : '/giphy.gif';
+  const loadingSrc = __DEVMODE__ ? fixedSrc : giphy;
   return (
     <Box sx={styles}>
-      <Image src={loadingSrc} fill alt="Loading" />
+      <Image unoptimized={true} src={loadingSrc} fill alt="Loading" />
     </Box>
   );
 }
